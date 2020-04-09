83da1eca64751af78aeb605d127d1dbb5721d37a
JayWebDevCom <jaiye.s@gmail.com>
2020-04-09 20:54:57 +0100

    Set GPIO to BCM mode, then return to BOARD mode in Switch on/off

--- com/components/Switch.py
+++ com/components/Switch.py
@@ -11,12 +11,16 @@ class Switch:
         self.pin = pin
 
     def on(self):
+        GPIO.setmode(GPIO.BCM)
         print(f"switching on {self.name}")
         GPIO.output(self.pin, 1)
+        GPIO.setmode(GPIO.BOARD)
 
     def off(self):
+        GPIO.setmode(GPIO.BCM)
         print(f"switching off {self.name}")
         GPIO.output(self.pin, 0)
+        GPIO.setmode(GPIO.BOARD)
 
     def __eq__(self, other):
         """Overrides the default implementation"""
