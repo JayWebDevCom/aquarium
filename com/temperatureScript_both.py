95d8e82265bbbc27d4da6b33f00cbd0c2fa43237
Jaiye Senbanjo <jaiye.s@gmail.com>
2020-04-12 16:44:15 +0100

    Add script to read both teperature sensors

---
+++ com/temperatureScript_both.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import time
+from components.TemperatureSensor import TemperatureSensor
+
+device_id_1 = "28-0300a279088e"
+device_id_2 = "28-0300a2792070"
+
+temperature_sensor_1 = TemperatureSensor("test sensor", device_id_1)
+temperature_sensor_2 = TemperatureSensor("test sensor", device_id_2)
+
+for i in range(0, 10):
+    time.sleep(1)
+    print(device_id_1 + ": " + str(temperature_sensor_1.get_temp()))
+    print(device_id_2 + ": " + str(temperature_sensor_2.get_temp()))
+
