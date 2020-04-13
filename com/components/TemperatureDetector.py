c94b6bb5da8da7147f401817ebfc2fedb208c3fd
JayWebDevCom <jaiye.s@gmail.com>
2020-04-13 21:48:12 +0100

    Improve TemperatureDetector logs

--- com/components/TemperatureDetector.py
+++ com/components/TemperatureDetector.py
@@ -1,4 +1,7 @@
 from components import TemperatureSensor
+from components.CustomFormatter import CustomFormatter
+import logging.config
+import yaml
 
 
 class TemperatureDetector:
@@ -17,7 +20,20 @@ class TemperatureDetector:
         self.sump_temp = sump_temp
         self.tank_temp = tank_temp
         self.limit = temperature_difference_limit
+        with open('log-config.yaml', 'r') as f:
+            config = yaml.safe_load(f.read())
+            logging.config.dictConfig(config)
+            self.logger = logging.getLogger(__name__)
+            ch = logging.StreamHandler()
+            ch.setLevel(logging.DEBUG)
+            ch.setFormatter(CustomFormatter())
+            self.logger.addHandler(ch)
 
     def within_range(self) -> bool:
-        temp_difference = self.sump_temp.get_temp() - self.tank_temp.get_temp()
-        return abs(temp_difference) <= self.limit
+        sump_temp = self.sump_temp.get_temp()
+        tank_temp = self.tank_temp.get_temp()
+        temp_difference = abs(sump_temp - tank_temp)
+
+        self.logger.info(f"temp difference: {round(temp_difference, 1)} "
+                         f"sump: {sump_temp} tank: {tank_temp}")
+        return temp_difference <= self.limit
