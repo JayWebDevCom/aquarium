fa8e445d6913c36eb3b28dd023a34e3d87b865d1
Jaiye Senbanjo <jaiye.s@gmail.com>
2020-04-14 14:27:11 +0100

    Improve temperatureScript_both

--- com/temperatureScript_both.py
+++ com/temperatureScript_both.py
@@ -12,7 +12,7 @@ device_id_2 = "28-0300a2792070"
 temperature_sensor_1 = TemperatureSensor("test sensor", device_id_1)
 temperature_sensor_2 = TemperatureSensor("test sensor", device_id_2)
 
-for i in range(0, 10):
+for i in range(0, 5):
     time.sleep(1)
-    logger.info(device_id_1 + ": " + str(temperature_sensor_1.get_temp()))
-    logger.info(device_id_2 + ": " + str(temperature_sensor_2.get_temp()))
+    logger.info("tank: " + str(temperature_sensor_1.get_temp()))
+    logger.info("sump: " + str(temperature_sensor_2.get_temp()))
