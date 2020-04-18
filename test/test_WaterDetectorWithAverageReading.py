c08a9bab31be7c3a4e55ea3e5599bbcc46d41ec4
Jaiye Senbanjo <jaiye.s@gmail.com>
2020-04-18 18:40:37 +0100

    Dynamically retrieve initial sump level
    * Add a log message to report initial full sump level reading
    * Introduce 0.2s sleep between initial start readings

--- test/test_WaterDetectorWithAverageReading.py
+++ test/test_WaterDetectorWithAverageReading.py
@@ -2,15 +2,15 @@ from unittest import TestCase, mock
 
 from com.components.LevelDetector import LevelDetector
 from com.components.LevelSensor import LevelSensor
-from components.AquariumLevels import AquariumLevels
+from components.LevelsBoundary import LevelsBoundary
 from components.ReadingsSanitizer import ReadingsSanitizer
 
 
 class TestWaterLevelDetectorWithAverage(TestCase):
     water_sensor = LevelSensor('water sensor')
-    aquarium_levels = AquariumLevels(20, 60)
-    sanitizer = ReadingsSanitizer(aquarium_levels, 0.1)
-    water_detector = LevelDetector('water detector', water_sensor, aquarium_levels, sanitizer)
+    levels_boundary = LevelsBoundary(20, 60)
+    sanitizer = ReadingsSanitizer(levels_boundary, 0.1)
+    water_detector = LevelDetector('water detector', water_sensor, levels_boundary, sanitizer)
 
     @mock.patch("com.components.LevelSensor.LevelSensor.get_level")
     def test_water_level_with_average(self, water_sensor):
