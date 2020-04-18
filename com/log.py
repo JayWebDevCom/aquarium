d8189767a12345915abeaefb813f2180706dbfbc
JayWebDevCom <jaiye.s@gmail.com>
2020-04-18 22:09:16 +0100

    Remove console hanlder from root logger

--- com/log.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from AquariumLogger import AquariumLogger
-
-logger = AquariumLogger()
-
-logger.info("this is an info message")
-logger.debug("this is a debug message")
-logger.error("this is an error message")
