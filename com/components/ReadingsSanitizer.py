14ad162487618e49ec2aa97d32be11c3de22d39c
JayWebDevCom <jaiye.s@gmail.com>
2020-05-30 14:06:36 +0100

    Accept floats in ReadingsSanitizer, LevelsBoundary

--- com/components/ReadingsSanitizer.py
+++ com/components/ReadingsSanitizer.py
@@ -4,20 +4,21 @@ from loguru import logger
 
 class ReadingsSanitizer:
     levels_boundary: LevelsBoundary
-    upper_bound: int
-    lower_bound: int
+    upper_bound: float
+    lower_bound: float
 
     def __init__(self, levels_boundary: LevelsBoundary, percentage_bound: float):
-        upper_bound = int(levels_boundary.empty_level * (1 + percentage_bound))
-        lower_bound = int(levels_boundary.full_level * (1 - percentage_bound))
-        self.acceptable_range = range(lower_bound, upper_bound)
+        self.upper_bound = levels_boundary.empty_level * (1 + percentage_bound)
+        self.lower_bound = levels_boundary.full_level * (1 - percentage_bound)
 
     def sanitize(self, readings_list) -> int:
         readings_list_copy = readings_list[:]
-        accepted_readings_iterator = filter(lambda reading: reading in self.acceptable_range, readings_list_copy)
+        accepted_readings_iterator = filter(lambda reading:
+                                            self.lower_bound <= reading <= self.upper_bound,
+                                            readings_list_copy)
         accepted_readings = list(accepted_readings_iterator)
 
         removed_readings = [item for item in readings_list_copy if item not in accepted_readings]
 
         logger.info(f"removed {len(removed_readings)} reading(s) {removed_readings}")
-        return int(sum(accepted_readings) / len(accepted_readings))
+        return round(sum(accepted_readings) / len(accepted_readings))
--- test/test_ReadingsSanitizer.py
+++ test/test_ReadingsSanitizer.py
@@ -6,19 +6,29 @@ from components.ReadingsSanitizer import ReadingsSanitizer
 
 class TestReadingsSanitizer(TestCase):
     percentage_boundary = 0.1
-    sanitizer = ReadingsSanitizer(LevelsBoundary(20, 40), percentage_boundary)
+    sanitizer = ReadingsSanitizer(LevelsBoundary(20.1, 40.7), percentage_boundary)
 
     def test_sanitize_basic(self):
         readings = [30, 30, 30, 30, 30, 30, 30, 30]
         self.assertEqual(30, self.sanitizer.sanitize(readings))
 
-    def test_sanitize(self):
+    def test_sanitize_int(self):
         params = [
             TestReadings(30, [30, 30, 30, 501, 30, 30, 30, 600, 30, 30, 500, 30, 30, 30, 700, 78, 30, 30, 30]),
             TestReadings(31, [0, 0, 0, 0, 1, 1, 1, 1, 3, 4, 5, 20, 30, 40, 34, 66]),
             TestReadings(30, [30, 30, 30, 30, 30, 30, 30, 30, 500, 819, 819]),
             TestReadings(30, [30, 30, 30, 30, 30, 30, 30, 30]),
             TestReadings(35, [28, 39, 40, 34])
+        ]
+
+        for reading in params:
+            self.assertEqual(reading.expected, self.sanitizer.sanitize(reading.readings))
+
+    def test_sanitize_float(self):
+        params = [
+            TestReadings(30, [30.0, 30.1, 30.2, 30.3, 30.4, 30.5, 30.6]),
+            TestReadings(31, [30.4, 30.5, 30.6, 30.7, 30.8]),
+            TestReadings(31, [30.4, 30.5, 30.6, 30.7, 30.8, 45.76]),
         ]
 
         for reading in params:
