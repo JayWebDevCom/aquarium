6a16f9e8f19bbd5342004745bab8b7e08bccc4e0
JayWebDevCom <jaiye.s@gmail.com>
2020-05-31 19:21:29 +0100

    Improve script variable naming

--- com/temperatureScript_both.py
+++ com/temperatureScript_both.py
@@ -2,11 +2,11 @@
 
 from components.TemperatureSensor import TemperatureSensor
 
-device_id_1 = "28-0300a279088e"
-device_id_2 = "28-0300a2792070"
+tank_device = "28-0300a279088e"
+sump_device = "28-0300a2792070"
 
-temperature_sensor_1 = TemperatureSensor("test sensor", device_id_1)
-temperature_sensor_2 = TemperatureSensor("test sensor", device_id_2)
+tank_temp_sensor = TemperatureSensor("test sensor", tank_device)
+sump_temp_sensor = TemperatureSensor("test sensor", sump_device)
 
 num = 5
 tank_temps_total = 0
@@ -15,8 +15,8 @@ sump_temps_total = 0
 tank_temp, sump_temp = 0, 0
 
 for i in range(0, num):
-    tank_temp = temperature_sensor_1.get_temp()
-    sump_temp = temperature_sensor_2.get_temp()
+    tank_temp = tank_temp_sensor.get_temp()
+    sump_temp = sump_temp_sensor.get_temp()
 
     tank_temps_total += tank_temp
     sump_temps_total += sump_temp
