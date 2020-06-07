f1ca9a7fc3ae5039b0f164450f5e96ed0ce15dce
Jaiye Senbanjo <jaiye.s@gmail.com>
2020-06-07 01:00:30 +0100

    Remove sdout logger handler

--- com/AquariumLogger.py
+++ com/AquariumLogger.py
@@ -1,7 +1,7 @@
 import sys
 from loguru import logger
 
-logger.add(sys.stdout, colorize=True, format="<green>{time:YYYY-MM-DD at HH:mm:ss}</green> | <level>{message}</level>")
+#logger.add(sys.stdout, colorize=True, format="<green>{time:YYYY-MM-DD at HH:mm:ss}</green> | <level>{message}</level>")
 logger.add("file.log", format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}", rotation="1 week")
 
 
