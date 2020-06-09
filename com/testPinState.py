c9723f48137dedf8da5d1d41b1b62a631b49b6a3
Jaiye Senbanjo <jaiye.s@gmail.com>
2020-06-09 18:37:42 +0100

    Improve test pin state script

--- com/testPinState.py
+++ com/testPinState.py
@@ -5,11 +5,11 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 @click.command()
-@click.option('--state', '-s', required=True, type=int, help='Enter a pin number to test state')
-def test_pin_state(state: int):
-    print(f'testing pin {state}')
-    GPIO.setup(state, GPIO.OUT)
-    pin_state = GPIO.input(state)
+@click.option('--pin', '-p', required=True, type=int, help='Enter a pin number to test state')
+def test_pin_state(pin: int):
+    print(f'testing pin {pin}')
+    GPIO.setup(pin, GPIO.OUT)
+    pin_state = GPIO.input(pin)
     if pin_state:
         print('on')
     else:
