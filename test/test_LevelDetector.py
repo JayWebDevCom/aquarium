46be07c92bfed2127ffbac2566ec227fa13d5497
Jaiye Senbanjo <jaiye.s@gmail.com>
2020-06-17 08:13:48 +0100

    Improve LevelDetector tests with allowance regarding ex

--- test/test_LevelDetector.py
+++ test/test_LevelDetector.py
@@ -11,8 +11,15 @@ class TestWaterLevelDetector(TestCase):
     water_sensor = LevelSensor('water sensor')
     levels_boundary = LevelsBoundary(20, 60)
     sanitizer = ReadingsSanitizer(levels_boundary, 0.5)
-    level_detector = LevelDetector('water detector', water_sensor, levels_boundary, sanitizer,
+    level_detector = LevelDetector('level detector [allowance 0]', water_sensor, levels_boundary, sanitizer,
                                    times_to_check_level=10, overfill_allowance=0)
+
+    level_detector_with_allowance = LevelDetector('level detector [allowance 1]', water_sensor, levels_boundary,
+                                                  sanitizer, times_to_check_level=10, overfill_allowance=1)
+
+    def _level_detector(self, name, overfill_allowance) -> LevelDetector:
+        return LevelDetector(name, self.water_sensor, self.levels_boundary, self.sanitizer,
+                             times_to_check_level=10, overfill_allowance=overfill_allowance)
 
     def test_percentage_changed_parameterized(self):
         params = {30: 25.0, 40: 50.0, 55: 87.5, 34.56: 36.4, 37.43: 43.57}
@@ -33,3 +40,17 @@ class TestWaterLevelDetector(TestCase):
             self.water_sensor.get_level = MagicMock(return_value=level)
             with self.assertRaises(UnexpectedWaterLevel):
                 self.level_detector.is_sump_full()
+
+    def test_sump_is_full_allowance(self):
+        params = {19.0: True, 20.0: True, 20.1: False, 40: False}
+        for water_level, expected in params.items():
+            self.water_sensor.get_level = MagicMock(return_value=water_level)
+            self.assertEqual(expected, self.level_detector_with_allowance.is_sump_full())
+
+    def test_unexpected_water_level_error_raised_allowance(self):
+        too_high = [15, 16, 17, 18, 18.9]
+        too_low = [61, 62, 63]
+        for level in too_high + too_low:
+            self.water_sensor.get_level = MagicMock(return_value=level)
+            with self.assertRaises(UnexpectedWaterLevel):
+                self.level_detector_with_allowance.is_sump_full()
