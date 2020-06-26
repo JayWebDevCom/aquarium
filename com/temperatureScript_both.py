5f26213ffa55c0e2e288e0df429f9d510343f6e2
Jaiye Senbanjo <jaiye.s@gmail.com>
2020-06-27 00:47:00 +0100

    Improve temperature script calculations

--- com/temperatureScript_both.py
+++ com/temperatureScript_both.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from functools import reduce
 
 from AquariumLogger import AquariumLogger
 from components.TemperatureSensor import TemperatureSensor
@@ -13,21 +14,19 @@ tank_temp_sensor = TemperatureSensor("test sensor", tank_device)
 sump_temp_sensor = TemperatureSensor("test sensor", sump_device)
 
 num = 3
-tank_temps_total = 0
-sump_temps_total = 0
-
-tank_temp, sump_temp = 0, 0
+tank_temps = []
+sump_temps = []
 
 for i in range(0, num):
-    tank_temp = tank_temp_sensor.get_temp()
-    sump_temp = sump_temp_sensor.get_temp()
+    tank_temps.append(tank_temp_sensor.get_temp())
+    sump_temps.append(sump_temp_sensor.get_temp())
 
-    tank_temps_total += tank_temp
-    sump_temps_total += sump_temp
+tank_temp_average = reduce(lambda x, y: (x + y), tank_temps) / num
+sump_temp_average = reduce(lambda x, y: (x + y), sump_temps) / num
 
 print("")
 logger.info(datetime.now().strftime("%H:%M"))
-logger.info(f"tank average: {'{:.2f}'.format(tank_temps_total/num)}")
-logger.info(f"sump average: {'{:.2f}'.format(sump_temps_total/num)}")
-logger.info(f"difference: {'{:.2f}'.format(abs(tank_temp-sump_temp))}")
+logger.info(f"tank average: {'{:.2f}'.format(tank_temp_average)}")
+logger.info(f"sump average: {'{:.2f}'.format(sump_temp_average)}")
+logger.info(f"difference: {'{:.2f}'.format(abs(tank_temp_average - sump_temp_average))}")
 
