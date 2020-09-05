b75eeb7d536fd351a517b2af2308d5ce07836d4b
JayWebDevCom <jaiye.s@gmail.com>
2020-09-05 22:16:28 +0100

    Introduce tested Configuration

---
+++ com/Configuration.py
@@ -0,0 +1,19 @@
+from yaml import load
+
+
+class Configuration:
+    def __init__(self, file_path: str):
+        with open(file_path, 'r') as stream:
+            try:
+                from yaml import CLoader as Loader, CDumper as Dumper
+                self.aquarium = load(stream)
+            except ImportError:
+                from yaml import Loader, Dumper
+
+    def water_change_times(self):
+        return self.aquarium['water_change_times']
+
+    def update_times(self):
+        return self.aquarium['update_times']
+
+
