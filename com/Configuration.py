0867925f99e84576d477a4e612369bf11d0bb38f
JayWebDevCom <jaiye.s@gmail.com>
2020-09-05 22:21:45 +0100

    Introduce tested Configuration

--- com/Configuration.py
+++ com/Configuration.py
@@ -2,11 +2,13 @@ from yaml import load
 
 
 class Configuration:
+    aquarium: dict
+
     def __init__(self, file_path: str):
         with open(file_path, 'r') as stream:
             try:
                 from yaml import CLoader as Loader, CDumper as Dumper
-                self.aquarium = load(stream)
+                self.aquarium = load(stream, Loader=Loader)
             except ImportError:
                 from yaml import Loader, Dumper
 
