7d6e46f821104de79a13f13b88a7e1c0ba138f1d
Jaiye Senbanjo <jaiye.s@gmail.com>
2020-09-12 19:26:16 +0100

    Update level sensor script

--- com/levelSensorWithTofScript.py
+++ com/levelSensorWithTofScript.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
-import time
 
 from AquariumLogger import AquariumLogger
 from components.LevelSensor import LevelSensor
 from components.TimeOfFlightLevelStrategy import TimeOfFlightLevelStrategy
 from functools import reduce
 
+global level_sensor
+
 logger = AquariumLogger()
 
 level_sensor = LevelSensor("test level sensor", TimeOfFlightLevelStrategy())
 
 num = 7
-levels = []
-
-for i in range(num):
-    levels.append(level_sensor.get_level())
+levels = [level_sensor.get_level() for i in range(num)]
 
 average_level = reduce(lambda x, y: (x + y), levels) / num 
 
