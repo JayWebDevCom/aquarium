2b58ef59b1404a797bffcb7155c28cf7f716b23c
JayWebDevCom <jaiye.s@gmail.com>
2020-10-10 21:16:29 +0100

    Make temp, level scripts use ProgressTracker

--- com/levelSensorWithTofScript.py
+++ com/levelSensorWithTofScript.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from AquariumLogger import AquariumLogger
+from ProgressBar import ProgressTracker
 from components.LevelSensor import LevelSensor
 from components.TimeOfFlightLevelStrategy import TimeOfFlightLevelStrategy
 from functools import reduce
@@ -14,7 +15,9 @@ level_sensor = LevelSensor("test level sensor", TimeOfFlightLevelStrategy())
 num = 7
 levels = [level_sensor.get_level() for i in range(num)]
 
-average_level = reduce(lambda x, y: (x + y), levels) / num 
+average_level = reduce(lambda x, y: (x + y), levels) / num
 
-logger.info(f"average level: {'{:.2f}'.format(average_level)}")
+progress_tracker = ProgressTracker("\033[0;0m")
+progress_tracker.write(f"average level: {'{:.2f}'.format(average_level)}")
+progress_tracker.finish()
 
--- com/temperatureScript_both.py
+++ com/temperatureScript_both.py
@@ -2,6 +2,7 @@
 from functools import reduce
 
 from AquariumLogger import AquariumLogger
+from ProgressBar import ProgressTracker
 from components.TemperatureSensor import TemperatureSensor
 from datetime import datetime
 
@@ -17,15 +18,20 @@ tank_temp_sensor = TemperatureSensor("test sensor", tank_device)
 sump_temp_sensor = TemperatureSensor("test sensor", sump_device)
 
 num = 1
-tank_temps = [tank_temp_sensor.get_temp() for i in range(num)]
-sump_temps = [sump_temp_sensor.get_temp() for i in range(num)]
+tank_temps = [tank_temp_sensor.get_temp() for _ in range(num)]
+sump_temps = [sump_temp_sensor.get_temp() for _ in range(num)]
 
 tank_temp_average = reduce(lambda x, y: (x + y), tank_temps) / num
 sump_temp_average = reduce(lambda x, y: (x + y), sump_temps) / num
 
+progress_tracker = ProgressTracker("\033[0;0m")
+
+date_time = datetime.now().strftime("%H:%M")
+tank_average = f"tank average: {'{:.2f}'.format(tank_temp_average)}"
+sump_average = f"sump average: {'{:.2f}'.format(sump_temp_average)}"
+difference = f"difference: {'{:.2f}'.format(abs(tank_temp_average - sump_temp_average))}"
+
 print("")
-logger.info(datetime.now().strftime("%H:%M"))
-logger.info(f"tank average: {'{:.2f}'.format(tank_temp_average)}")
-logger.info(f"sump average: {'{:.2f}'.format(sump_temp_average)}")
-logger.info(f"difference: {'{:.2f}'.format(abs(tank_temp_average - sump_temp_average))}")
+progress_tracker.write(f"{tank_average}, {sump_average}, {difference}")
+progress_tracker.finish()
 
