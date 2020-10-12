43b1420ed9a5c8ffdf36d067a7d8e7bbcd14b994
JayWebDevCom <jaiye.s@gmail.com>
2020-10-12 17:50:12 +0100

    Make temp script use config, progress_tracker

--- com/temperatureScript_both.py
+++ com/temperatureScript_both.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
+import os
+from datetime import datetime
 from functools import reduce
 
-from AquariumLogger import AquariumLogger
-from ProgressBar import ProgressTracker, Style
+from Configuration import Configuration
+from Progress import ProgressTracker, Style
 from components.TemperatureSensor import TemperatureSensor
-from datetime import datetime
 
-logger = AquariumLogger()
-
-sump_device = "28-01191c5f02ae"
-tank_device = "28-01191c6c5b42"
+current_dir = os.path.dirname(os.path.abspath(__file__))
+config_file = "config.yaml"
+c_file = f"{current_dir}/{config_file}"
+config = Configuration(c_file)
 
 global tank_temp_sensor
 global sump_temp_sensor
-
-tank_temp_sensor = TemperatureSensor("test sensor", tank_device)
-sump_temp_sensor = TemperatureSensor("test sensor", sump_device)
+tank_temp_sensor = TemperatureSensor("tank sensor", config.get("tank_temp_device_id"))
+sump_temp_sensor = TemperatureSensor("sump sensor", config.get("sump_temp_device_id"))
 
 num = 1
 tank_temps = [tank_temp_sensor.get_temp() for _ in range(num)]
@@ -24,13 +24,11 @@ sump_temps = [sump_temp_sensor.get_temp() for _ in range(num)]
 tank_temp_average = reduce(lambda x, y: (x + y), tank_temps) / num
 sump_temp_average = reduce(lambda x, y: (x + y), sump_temps) / num
 
-progress_tracker = ProgressTracker(Style.BOLD)
-
 date_time = datetime.now().strftime("%H:%M")
-tank_average = f"tank average: {'{:.2f}'.format(tank_temp_average)}"
-sump_average = f"sump average: {'{:.2f}'.format(sump_temp_average)}"
+tank_average = f"tank average: {'{:.2f}c'.format(tank_temp_average)}"
+sump_average = f"sump average: {'{:.2f}c'.format(sump_temp_average)}"
 difference = f"difference: {'{:.2f}'.format(abs(tank_temp_average - sump_temp_average))}"
 
 print("")
-progress_tracker.write(f"{date_time}: {tank_average}, {sump_average}, {difference}")
-progress_tracker.finish()
+progress_tracker = ProgressTracker()
+progress_tracker.write_ln(f"{Style.BOLD}{date_time}: {tank_average}, {sump_average}, {difference}")
