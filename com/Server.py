375936b8148c7c1ef5ceeb718e07197f39c5a750
Jaiye Senbanjo <jaiye.s@gmail.com>
2023-12-29 15:18:25 +0000

    Fix self referentiality

--- com/Server.py
+++ com/Server.py
@@ -3,25 +3,25 @@ from flask import Flask
 from Controller import Controller
 
 
-app = Flask(__name__)
-
 class Server:
     def __init__(
             self,
             controller: Controller):
         self.controller = controller
+        self.app = Flask(__name__)
+        # routes
+        self.app.route('/')(self.ok)
+        self.app.times('/times')(self.times)
+        self.app.breakdown('/breakdown')(self.breakdown)
 
     def start(self):
         app.run(debug = True)
 
-    @app.route('/')
     def ok():
         return "OK"
 
-    @app.route('/times')
     def times(self):
         return self.controller.times()
 
-    @app.route('/breakdown')
     def breakdown(self):
         return self.controller.breakdown()
