b0f62bfc86c80579e431abb896e3ebefe332a66c
Jaiye Senbanjo <jaiye.s@gmail.com>
2024-01-03 16:54:45 +0000

    refactor

--- com/Server.py
+++ com/Server.py
@@ -1,4 +1,5 @@
 import json
+
 from flask import Flask, Response, request
 from Configuration import Configuration
 from Controller import Controller
@@ -6,12 +7,13 @@ from Controller import Controller
 
 class Server:
     JSON = 'application/json'
+
     def __init__(
             self,
             controller: Controller,
             configuration: Configuration,
-            host = '0.0.0.0',
-            port = 5000):
+            host='0.0.0.0',
+            port=5000):
         self.controller = controller
         self.configuration = configuration
         self.app = Flask(__name__)
@@ -19,41 +21,48 @@ class Server:
         self.port = port
 
     def start(self):
-        self.app.add_url_rule('/', view_func=self.ok)
+        self.app.add_url_rule('/', view_func=Server.ok)
         self.app.add_url_rule('/config', methods=['GET', 'POST'], view_func=self.config)
         self.app.add_url_rule('/times', methods=['GET', 'POST'], view_func=self.times)
         self.app.add_url_rule('/breakdown', methods=['GET'], view_func=self.breakdown)
 
         self.app.run(host=self.host, port=self.port)
 
-    def ok(self):
+    @staticmethod
+    def ok():
         return Response("OK", mimetype='text/xml')
 
     def times(self):
+        content_type = request.headers['Content-Type']
         if request.method == 'GET':
             up_to_date_config_data = Configuration(self.configuration.file_path).data()
             wc_times_json = json.dumps(up_to_date_config_data['water_change_times'])
-            return Response(wc_times_json, mimetype=Server.JSON)
+            return Server.response_of(wc_times_json, Server.JSON, 200)
         elif request.headers['Content-Type'] == 'application/json':
             up_to_date_config_data = Configuration(self.configuration.file_path).data()
             up_to_date_config_data['water_change_times'] = request.get_json()['water_change_times']
             self.configuration.write_data(up_to_date_config_data)
             response_json = json.dumps(up_to_date_config_data['water_change_times'])
-            return Response(response_json, mimetype=Server.JSON, status=201,)
+            return Server.response_of(response_json, Server.JSON, 201)
         else:
-            return Response(f"content-type not supported {content_type}", status=500,)
+            return Server.response_of(f"content-type not supported {content_type}", Server.JSON, status=500)
 
     def config(self):
+        content_type = request.headers['Content-Type']
         if request.method == 'GET':
             up_to_date_config_data = Configuration(self.configuration.file_path).data()
             config_json = json.dumps(up_to_date_config_data)
-            return Response(config_json, mimetype=Server.JSON)
-        elif request.headers['Content-Type'] == 'application/json':
-             data = request.get_json()
-             self.configuration.write_data(data)
-             return Response(json.dumps(data), mimetype=Server.JSON, status=201,)
+            return Server.response_of(config_json, Server.JSON, 200)
+        elif content_type == 'application/json':
+            data = request.get_json()
+            self.configuration.write_data(data)
+            return Server.response_of(json.dumps(data), mimetype=Server.JSON, status=201)
         else:
-             return Response(f"content-type not supported {content_type}", mimetype='text/xml', status=500,)
+            return Server.response_of(f"content-type not supported {content_type}", 'text/xml', 500)
 
     def breakdown(self):
-        return Response(json.dumps(self.controller.breakdown()), mimetype=Server.JSON)
+        return Server.response_of(json.dumps(self.controller.breakdown()), Server.JSON, 200)
+
+    @staticmethod
+    def response_of(message: str, mimetype: str, status: int):
+        return Response(message, mimetype=mimetype, status=status)
