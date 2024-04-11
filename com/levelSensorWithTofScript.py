2fcbf87fe22c67030f7406486e0289f11a8ba6d3
Jaiye Senbanjo <jaiye.s@gmail.com>
2024-04-11 13:28:18 +0100

    Print full percentage in update

--- com/levelSensorWithTofScript.py
+++ com/levelSensorWithTofScript.py
@@ -11,12 +11,18 @@ global level_sensor
 level_sensor = LevelSensor("test level sensor", TimeOfFlightLevelStrategy())
 progress_tracker = ProgressTracker()
 
-num = 7
-levels = [level_sensor.get_level() for i in range(num)]
+number_of_times_to_check_level = 7
+full_level = 25
+water_change_span = 20.5
 
-average_level = reduce(lambda x, y: (x + y), levels) / num
+levels = [level_sensor.get_level() for i in range(number_of_times_to_check_level)]
+average_level = reduce(lambda x, y: (x + y), levels) / number_of_times_to_check_level
+
+difference = average_level - full_level
+percent_full = 100 - (difference / water_change_span) * 100
+
 date_time = datetime.now().strftime("%H:%M")
 
 progress_tracker.write_ln(f"{Style.WHITE}{Style.BOLD}{date_time}:{Style.RESET}{Style.GREEN} "
-                          f"level: {Style.BOLD}{Style.WHITE}{'{:.2f}'.format(average_level)}")
-
+                          f"level: {Style.BOLD}{Style.WHITE}{'{:.2f}'.format(average_level)}{Style.RESET}{Style.GREEN} "
+                          f"full percentage: {Style.BOLD}{Style.WHITE}{'{:.2f}'.format(percent_full)} ")
