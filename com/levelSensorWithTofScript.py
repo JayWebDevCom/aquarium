04af08b75e3c6e3952888a7d5975a4cadb9089ad
Jaiye Senbanjo <jaiye.s@gmail.com>
2024-04-12 15:32:19 +0100

    Use config object in update script

--- com/levelSensorWithTofScript.py
+++ com/levelSensorWithTofScript.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3
+import os
 from datetime import datetime
 from functools import reduce
 
 from Progress import ProgressTracker, Style
 from components.LevelSensor import LevelSensor
 from components.TimeOfFlightLevelStrategy import TimeOfFlightLevelStrategy
+from com.Configuration import Configuration
 
 global level_sensor
 
 level_sensor = LevelSensor("test level sensor", TimeOfFlightLevelStrategy())
 progress_tracker = ProgressTracker()
 
-number_of_times_to_check_level = 7
-full_level = 25
-water_change_span = 20.5
+current_dir = os.path.dirname(os.path.abspath(__file__))
+configuration_file_name = "config.yaml"
+configuration_file_path = f"{current_dir}/{configuration_file_name}"
+config = Configuration(configuration_file_path)
 
-levels = [level_sensor.get_level() for i in range(number_of_times_to_check_level)]
-average_level = reduce(lambda x, y: (x + y), levels) / number_of_times_to_check_level
+times_to_check_level = config.get("times_to_check_level")
+full_level = config.get("full_level")
+water_change_span = config.get("water_change_span")
+
+levels = [level_sensor.get_level() for i in range(times_to_check_level)]
+average_level = reduce(lambda x, y: (x + y), levels) / times_to_check_level
 
 difference = average_level - full_level
 percent_full = 100 - (difference / water_change_span) * 100
